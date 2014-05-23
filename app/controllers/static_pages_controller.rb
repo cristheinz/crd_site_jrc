ef116ae4f514d8ea1f5bf34514e31eb2a4bd2b82
Christiano Domingues <cristheinz@gmail.com>
2014-05-23 13:54:02 +0100

    first commit

---
+++ app/controllers/static_pages_controller.rb
@@ -0,0 +1,10 @@
+class StaticPagesController < ApplicationController
+  def home
+  end
+
+  def curso
+  end
+
+  def contacto
+  end
+end
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CrdSiteJrc::Application.config.secret_token = '5aa1b3380306911fa38383c6b64d3b921fb379ce6eb5b34666ab08e8e559260b5f8b533e6ba7a7cb07445a99937a6463cdcfedff4dfd190e8e70435518f350d3'
